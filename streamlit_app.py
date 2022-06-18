53192bc2fc605a0234b7d2275f40738f0c66e9d3
fiftyeightandeight <alexd@alexgo.io>
2022-06-18 21:26:20 +0800

    working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
@@ -6,16 +5,6 @@ import streamlit as st
 import requests
 import json 
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 url = "http://api.alexlab.co/v1/price/"
 token_list = [ "token-wstx", "age000-governance-token", "token-wxusd", "token-wbtc", "token-wusda", "token-wban", "token-wslm", "token-wmia", "token-wnycc", "auto-alex" ]
 
@@ -25,24 +14,23 @@ def safe_execute(response):
     except Exception:
         return -1
 
+instant_prices = []
+resilient_prices = []
+external_prices = []
+for x in token_list:
+    res = requests.get(url + x)
+    instant_prices.append(safe_execute(res.text))
+    res = requests.get(url + x + '-twap')
+    resilient_prices.append(safe_execute(res.text))
+    res = requests.get(url + x + '-external')
+    external_prices.append(safe_execute(res.text))
+
+data = {}
+data['Spot'] = instant_prices
+data['TWAP'] = resilient_prices
+data['External'] = external_prices
+
+df = pd.DataFrame(data = data, index = token_list)
+
 with st.echo(code_location='below'):
-
-    instant_prices = []
-    resilient_prices = []
-    external_prices = []
-    for x in token_list:
-        res = requests.get(url + x)
-        instant_prices.append(safe_execute(res.text))
-        res = requests.get(url + x + '-twap')
-        resilient_prices.append(safe_execute(res.text))
-        res = requests.get(url + x + '-external')
-        external_prices.append(safe_execute(res.text))
-    
-    data = {}
-    data['Spot'] = instant_prices
-    data['TWAP'] = resilient_prices
-    data['External'] = external_prices
-
-    df = pd.DataFrame(data = data, index = token_list)
-
     st.dataframe(df)
